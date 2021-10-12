4abffc46b0928e4bb656ad8eb077dd3348807cc3
Bruno1083 <cavalcante1083@gmail.com>
2021-10-12 00:32:33 -0300

    projeto inicial

---
+++ src/main/java/com/cavalcante/apicafedamanha/ApiCafeDaManhaApplication.java
@@ -0,0 +1,13 @@
+package com.cavalcante.apicafedamanha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCafeDaManhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCafeDaManhaApplication.class, args);
+	}
+
+}
