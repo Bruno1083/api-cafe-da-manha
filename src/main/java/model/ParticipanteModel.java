7b469bfcb6b18140d7912de0813ae610c8a2919f
Bruno1083 <cavalcante1083@gmail.com>
2021-10-13 16:36:55 -0300

    model

---
+++ src/main/java/model/ParticipanteModel.java
@@ -0,0 +1,28 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity(name= "Participante")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ParticipanteModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(unique = true, nullable = false, length = 20)
+    private String cpf;
+
+    @Column(unique = true, nullable = false)
+    private String[] item;
+}
---
+++ src/main/java/model/ParticipanteResponse.java
@@ -0,0 +1,15 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ParticipanteResponse {
+    private ParticipanteModel particimanpe;
+    private String mengagem;
+}
